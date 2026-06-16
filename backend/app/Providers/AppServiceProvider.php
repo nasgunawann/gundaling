2af77ac5e77adb42afaa76b03f78fc504e394f74
Nasgun <nasgunawann@gmail.com>
2026-06-16 18:20:27 +0700

    fix: change broadcasting auth route middleware to auth:sanctum to support token authorization

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -19,6 +19,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        \Illuminate\Support\Facades\Broadcast::routes(['middleware' => ['api', 'auth:sanctum']]);
+        require base_path('routes/channels.php');
     }
 }
